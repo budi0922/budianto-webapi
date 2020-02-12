9c23139576e31cc80232317c90b69d49bfdd7731
unknown <budi0922>
2020-02-11 23:36:32 -0800

    init

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Student;
+
+class StudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // tampilkan student
+        return Student::all();
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // membuat / [post]
+        $student = new Student;
+
+        $student->nama = $request->nama;
+        $student->jk = $request->jk;
+        $student->save();
+
+        return "Data Berhasil Masuk";
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        // edit / put student
+        $student = Student::find($id);
+        $student->nama = $request->nama;
+        $student->jk = $request->jk;
+        $student->save();
+
+        return "Data Berhasil di Ubah";
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        // hapus student
+        $student = Student::find($id);
+        $student->delete();
+
+        return "Data Berhasil di Hapus";
+    }
+}
